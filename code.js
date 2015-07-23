4e6300a0da8be6636070ed199e54275629fde003
penerus <penerus@users.noreply.github.com>
2015-07-24 01:01:13 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var linkarray = [
-"Free <a href='http://backlinkxchange.blogspot.com' title='Free Backlink Exchange' target='_blank'>BacklinkX</a>change + <a href='http://astavera.blogspot.com' title='Astavera' target='_blank'>Astavera</a>",
-"Free <a href='http://backlinkxchange.blogspot.com' title='Free Backlink Exchange' target='_blank'>BacklinkX</a>change + <a href='http://padhangwengi.blogspot.com' title='Padhang Wengi' target='_blank'>Padhang Wengi</a>" // No comma after last entry
+"Free <a href='http://backlinkxchange.blogspot.com/?track' title='Free Backlink Exchange' target='_blank'>BacklinkX</a>change + <a href='http://astavera.blogspot.com' title='Astavera' target='_blank'>Astavera</a>",
+"Free <a href='http://backlinkxchange.blogspot.com/?track' title='Free Backlink Exchange' target='_blank'>BacklinkX</a>change + <a href='http://padhangwengi.blogspot.com' title='Padhang Wengi' target='_blank'>Padhang Wengi</a>" // No comma after last entry
 ];
 
 function RndLink() {
