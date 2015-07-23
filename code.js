af4706b0e545adb38b9711b6cc22cc02766a843a
penerus <penerus@users.noreply.github.com>
2015-07-23 09:27:14 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,11 @@
+var linkarray = [
+"Free <a href='http://backlinkxchange.blogspot.com' title='Free Backlink Exchange' target='_blank'>BacklinkXchange</a> + <a href='http://astavera.blogspot.com' title='Astavera' target='_blank'>Astavera</a>",
+"Free <a href='http://backlinkxchange.blogspot.com' title='Free Backlink Exchange' target='_blank'>BacklinkXchange</a> + <a href='http://padhangwengi.blogspot.com' title='Padhang Wengi' target='_blank'>Padhang Wengi</a>" // No comma after last entry
+];
+
+function RndLink() {
+var run= Math.floor(Math.random()*linkarray.length);
+document.getElementById('BacklinkX').innerHTML=linkarray[run];
+}
+onload = function() { RndLink(); 
+}
