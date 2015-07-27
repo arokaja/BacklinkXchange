3eb93a3c41325838cca7a05650206ea78fb03c44
Razz <penerus@users.noreply.github.com>
2015-07-28 01:36:00 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 var linkarray = [
 "<a href='http://astavera.blogspot.com' title='Astavera' target='_blank'>Astavera</a>",
-"<a href='http://padhangwengi.blogspot.com' title='Padhang Wengi' target='_blank'>Padhang Wengi</a>" // No comma after last entry
+"<a href='http://padhangwengi.blogspot.com' title='Padhang Wengi' target='_blank'>Padhang Wengi</a>",
+"<a href='http://thequran-online.blogspot.com' title='Al-Quran Online' target='_blank'>Al-Quran Online</a>" // No comma after last entry
 ];
 
 function RndLink() {
